e8546143177462e22b226153c2799e5d2c0cd566
MKarthik1729 <mkarthik1729@gmail.com>
2025-11-05 10:13:09 +0530

    moved to staging

--- src/assets/config.tsx
+++ src/assets/config.tsx
@@ -5,11 +5,11 @@ type Environment = "dev" | "staging" | "prod";
 export const url = (): string => {
   const links: Record<Environment, string> = {
     dev: "http://localhost:3000/api",
-    staging: "https://staging.example.com/api",
+    staging: "https://daanpitara-1.onrender.com/api",
     prod: "https://daanpitara.onrender.com/api",
   };
 
-  const env = "prod";
+  const env = "staging";
 
   return links[env];
 };
